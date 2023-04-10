30c93277caad5eae973c58144a43b01a7b3a5c57
Rohit <rohitamar@gmail.com>
2023-04-09 21:28:20 -0400

    trying this again

--- src/main.go
+++ src/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/rohitamar/load-balancer/serverFarm"
 )
 
 func main() {
-	ports := [3]string{"8080", "8081", "8082"}
-	serverFarm.startServerFarm(ports)
+	fmt.Println("Hello World")
 }
